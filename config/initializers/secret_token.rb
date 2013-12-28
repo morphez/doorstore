91f7eed668f654a9d74f3a47f29e63dac8e1b83a
morphez <aushakov.pk@gmail.com>
2013-12-28 17:26:53 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorstore::Application.config.secret_token = '04050a338efdece84062e21880290b2166d08c5ea7ee27d5612f7c4ddd2c958a9963e53fb95339ffec855b1dd93873a01de77dfbd85b7b4ab55adeacdbb05fa3'
